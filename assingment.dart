e6397f759cfc59f7310d80bd70f1344eea213c94
ArhamSiddiqui1 <arhamkashif16@gmail.com>
2024-05-31 22:00:40 +0500

    assingment

---
+++ assingment.dart
@@ -0,0 +1,32 @@
+
+void main(){
+// List names =["ali","zaid","aman","abdul","uzair"];
+// print(names);
+// List days =[];
+// days.addAll(["monday","tuesday"]);
+// days.add('tuesday');
+// days.add('wednesday');
+// days.add('thursday');
+// days.add('friday');
+// days.add('saturday');
+// days.add('sunday');
+// print (days);
+// for(int i=0 ;i<7;i++){
+// days.removeLast();
+// }
+// print (days);
+// List nums =[9,2,23,5,6,13,8,45,26,10];
+// nums.sort();
+// print(nums.last);
+// Map phone1 =
+// {'num1': 2344 , 'num2': 234,'num2': 23456}
+// ;
+//   phone1.values.where((value) => value > 999).forEach((value) {
+//     print(value);
+//   });
+
+
+
+
+
+}
---
+++ tempCodeRunnerFile.dart
@@ -0,0 +1,9 @@
+List days =[];
+// days.addAll(["monday","tuesday"]);
+// days.add('tuesday');
+// days.add('wednesday');
+// days.add('thursday');
+// days.add('friday');
+// days.add('saturday');
+// days.add('sunday');
+// print (days);
